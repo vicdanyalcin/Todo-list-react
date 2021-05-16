51ae5955023dc7fbb026f5a8978a44613c5dbafd
vicdanyalcin <vicdanyalcin.vyn@gmail.com>
2021-05-16 19:31:49 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,46 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
-
-function TodoForm({ addTodo }) {
-  const [value, setValue] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-    setValue("");
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      ></input>
-      <button className="addtodos-btn" onSubmit={addTodo}>
-        Add Todo
-      </button>
-    </form>
-  );
-}
-function Todo({ todo, index, completeTodo, removeTodo }) {
-  return (
-    <div
-      className="todo"
-      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
-    >
-      {todo.text}
-
-      <div>
-        <button className="complete-btn" onClick={() => completeTodo(index)}>
-          Complete
-        </button>
-        <button onClick={() => removeTodo(index)}>x</button>
-      </div>
-    </div>
-  );
-}
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -55,7 +16,7 @@ function App() {
   };
   const completeTodo = (index) => {
     const newTodos = [...todos];
-    newTodos[index].isCompleted = true;
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
     setTodos(newTodos);
   };
   const removeTodo = (index) => {
---
+++ src/components/TodoForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button className="addtodos-btn" onSubmit={addTodo}>
+        Add Todo
+      </button>
+    </form>
+  );
+};
+
+export default TodoForm;
